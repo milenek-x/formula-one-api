cd405b41b058e59e5b26155f8b6ca1f679bedb46
milenek-x <muneebasmone@gmail.com>
2025-04-06 13:50:07 +0530

    Reverting cron jobs since vercel free tier limitations

--- update_data.py
+++ update_data.py
@@ -1,9 +1,12 @@
-# api/update_data.py
 from update_firestore_data import update_all
 
-def handler(request):
-    update_all()
-    return {
-        "statusCode": 200,
-        "body": "Firestore updated successfully."
-    }
+def handler(request, response):
+    try:
+        update_all()
+        return response.status(200).json({
+            "message": "Firestore updated successfully."
+        })
+    except Exception as e:
+        return response.status(500).json({
+            "error": str(e)
+        })
