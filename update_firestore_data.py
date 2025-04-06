e63320ee20698a99fdcac855a161cbfde1b4cf4a
milenek-x <muneebasmone@gmail.com>
2025-04-06 17:53:31 +0530

    Updating firebase logic in update_firebase_data.py

--- update_firestore_data.py
+++ update_firestore_data.py
@@ -72,7 +72,8 @@ def update_circuits():
 
         try:
             circuit_info = get_circuit_info(race_url)
-            db.collection('races').document(doc.id).update({'circuit': circuit_info})
+            db.collection('races').where('race_id', '==', race_id).limit(1).update({'circuit': circuit_info})
+            # db.collection('races').document(doc.id).update({'circuit': circuit_info})
             print(f"Updated circuit for race: {race.get('name')}")
         except Exception as e:
             print(f"Failed to update circuit for race {race.get('name')}: {e}")
@@ -90,7 +91,8 @@ def update_sessions():
         try:
             sessions = get_race_sessions(race_url)
             if sessions:
-                db.collection('races').document(doc.id).update({'sessions': sessions})
+                db.collection('races').where('race_id', '==', race_id).limit(1).update({'sessions': sessions})
+                # db.collection('races').document(doc.id).update({'sessions': sessions})
                 print(f"Updated sessions for race: {race.get('name')}")
         except Exception as e:
             print(f"Failed to update sessions for race {race.get('name')}: {e}")
